08dc7a90a32a338864faf08dfe7ef84a5dbad47e
Angel <angelclaudio@outlook.com>
2024-09-13 22:57:15 -0400

    Update CountFunctionDemo.sql

--- CountFunctionDemo.sql
+++ CountFunctionDemo.sql
@@ -1,3 +1,4 @@
+  USE AdventureWorks2019
   -- Demo of COUNT function in T-SQL, which returns the number of items found in a group.
 
   -- Aggregation Function Syntax: COUNT ( { [ [ ALL | DISTINCT ] expression ] | * } )
@@ -48,8 +49,7 @@
   if you apply the DISTINCT argument (which will forbid duplicates), you will get only 1 count.
   */
 
-  -- Bonus Question 3: What are analytic functions - functions
-  -- which bring back a group of rows to be further analyzed per record.
+  -- Bonus Question 3: What are analytic functions?
 
 SELECT DISTINCT d.[Name]
     , MIN(eph.Rate) OVER (PARTITION BY edh.DepartmentID) AS MinSalary
@@ -69,4 +69,32 @@ ORDER BY Name;
 - Analytics do not reduce the number of rows returned.
 - Aggregate functions, may be considered "Analytic" when used with "Over", and there are some functions that are purely "Analytic".
 - Aggregate function which are used as "Analytic" function will mostly use the PARTITION BY sub-clause of OVER.
+*/
+
+-- Bonus Question 4: How can I better study T-SQL independently (or in my interpretation, make it fun)?
+
+/*
+ The key to increasing your understanding of anything, is by increasing your "vocabulary" in that thing.
+
+ YouTube and other platforms of learning exploit low level student by increasing the time to teach a subject,
+ by explaining what should already be understood to the student without detailing the nuances, and so you 
+ never really own the material, you are given specific common solutions with a "general understanding" of how it works.
+
+ YouTube is not bad for a quick and dirty, but how do we become outright sustainable and clean?
+
+ - Establish the "manual" (yes "RTFM" is legit), sources of information that are official or recognized authorities.
+
+ - Learn how to "read the manual", usually found in a preface or URL to the content explaining conventions of the material, e.g.:
+ https://learn.microsoft.com/en-us/sql/t-sql/language-elements/transact-sql-syntax-conventions-transact-sql?view=sql-server-ver16&redirectedfrom=MSDN&tabs=code
+
+ - The beginning takes time and effort since you are building the base of your knowledge - e.g., this is why learning a new language WELL is super difficult for many,
+   people mostly learn just enough to get by (quick and dirty).
+
+ - More complex topics become small additions to an algamation of things you already know, this is where learning becomes quicker and fun while 
+   keeping thing you already learned easier to access from memory - e.g., you learn a language well, and now reading a book, newspaper, or even doing a crossword 
+   considered "difficult" because you need to understand a language well, is now "fun".
+
+ - If things are becoming fun, continue doing them. Anything we don't practice loses it's form with us, 
+ - we are built to survive, automatically prioritizing our resources (mental and physical capacities) at whatever we do continuously.
+
 */
